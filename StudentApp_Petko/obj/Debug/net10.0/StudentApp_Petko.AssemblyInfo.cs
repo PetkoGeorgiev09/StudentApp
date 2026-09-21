12538fc06e909d79882619b5dbe20c99132975df
PetkoGEorgiev09 <petkogeorgiev09@gmail.com>
2026-09-21 11:42:31 +0300

    Добавен е клас Student

--- StudentApp_Petko/obj/Debug/net10.0/StudentApp_Petko.AssemblyInfo.cs
+++ StudentApp_Petko/obj/Debug/net10.0/StudentApp_Petko.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentApp_Petko")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9cf5913344bd7bce43d365c934b5e39df5abbd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentApp_Petko")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentApp_Petko")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
